8befdfa6be7dde216c05418ac282c219eecba15a
felixjunghans <kontakt@felixjunghans.de>
2021-10-19 23:03:09 +0200

    fix bugs

--- lib/src/cache_manager_interface.dart
+++ lib/src/cache_manager_interface.dart
@@ -1,9 +1,7 @@
 import 'dart:typed_data';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_image/firebase_image.dart';
 import 'package:firebase_image/src/image_object.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 FirebaseImageCacheManager getFirebaseImageCacheManager(
         CacheRefreshStrategy cacheRefreshStrategy) =>
@@ -19,8 +17,6 @@ abstract class FirebaseImageCacheManager {
   Future<bool> checkDatabaseForEntry(FirebaseImageObject object);
 
   Future<FirebaseImageObject?> get(String uri, FirebaseImage image);
-
-  Reference getImageRef(FirebaseImageObject object, FirebaseApp? firebaseApp);
 
   Future<List<FirebaseImageObject>> getAll();
 
