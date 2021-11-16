6a4be9ad36f7e0b56d4e0a247e41604fbeb939ba
felixjunghans <kontakt@felixjunghans.de>
2021-11-16 15:47:52 +0100

    export cache manager

--- lib/firebase_image.dart
+++ lib/firebase_image.dart
@@ -1,4 +1,7 @@
 library firebase_image;
 
-export 'src/firebase_image.dart';
+export 'src/cache_manager_interface.dart'
+    if (dart.library.html) 'src/cache_manager_web.dart'
+    if (dart.library.io) 'src/cache_manager.dart';
 export 'src/cache_refresh_strategy.dart';
+export 'src/firebase_image.dart';
