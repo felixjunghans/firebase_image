040d0a8c0ee89922c6418d7776319cab11878abc
felixjunghans <kontakt@felixjunghans.de>
2021-11-17 14:22:33 +0100

    export image object

--- lib/firebase_image.dart
+++ lib/firebase_image.dart
@@ -5,3 +5,4 @@ export 'src/cache_manager_interface.dart'
     if (dart.library.io) 'src/cache_manager.dart';
 export 'src/cache_refresh_strategy.dart';
 export 'src/firebase_image.dart';
+export 'src/image_object.dart';
